87da6edbb3c14f682d744522777eaac8b606d88d
RonGarlit <rongarlit@live.com>
2018-07-18 15:03:51 -0400

    Finsihed Singleton js and ejs. Also edited Prototype.ejs

--- public/scripts/singleton.js
+++ public/scripts/singleton.js
@@ -2,16 +2,17 @@
 // singleton.js
 //==============================================
 
-
+// Singleton object is implemented as an immediate anonymous function
+// Eek!! This is in the global space
 var Singleton = (function () {
-
+    // Private variable
     var instance;
-
+    // Create our main and only real instance!!!!!
     function createInstance() {
         var object = new Object("I am the instance");
         return object;
     }
-
+    // Public Method to get instance and if it doesn't exist create one. 
     return {
         getInstance: function () {
             if (!instance) {
@@ -22,14 +23,16 @@ var Singleton = (function () {
         }
     };
 })();
-
-
-function run() {
-
+//==============================================    
+// run_Singleton()
+//============================================== 
+function run_Singleton() {
+    // separately create two instance variables 
+    // using the getInstance method in our module
     var instance1 = Singleton.getInstance();
     var instance2 = Singleton.getInstance();
-
-    alert("Same instance? " + (instance1 === instance2));  
+    // AlertBox with results comparing whether both instances are the same.
+    alert("Same instance? " + (instance1 === instance2));
 }
 
 //==============================================
@@ -47,8 +50,29 @@ function run() {
 // http://esbueno.noahstokes.com/post/77292606977/self-executing-anonymous-functions-or-how-to-write
 //==============================================
 
+//==============================================
+// Singleton
+// JavaScript does not support class-based 
+// inheritance therefore the abstract classes.
+// we must ensure this consistency ourselves
+// that properties and methods match!!!
+//----------------------------------------------
+// Note here again in OOP we would define a interface
+// or abstract classes.   
+//==============================================
 
-var Patterns = {
+//==============================================
+// Create the "Classic" namespace with the
+// Singleton namespace which is also in
+// the singleton.js
+//==============================================
+// The Namespace pattern is applied to keep the code 
+// out of the global namespace.
+//==============================================
+
+// Add our Singleton Module on the "Classic" on 
+// main namespace called JsGangOfFour
+var JsGangOfFour = {
     namespace: function (name) {
         var parts = name.split(".");
         var ns = this;
@@ -61,38 +85,50 @@ var Patterns = {
         return ns;
     }
 };
-
-Patterns.namespace("Classic").Singleton = (function () {
-
+//==============================================    
+// Here our Singleton module is added to namespace
+//============================================== 
+JsGangOfFour.namespace("Classic").Singleton = (function () {
+    //==============================================    
+    // Revealing Module named Singleton encapsulates 
+    // all of Singleton functions and properties.
+    //==============================================  
+    // Delcare private variable
     var instance;
-
+    // Create our main and only real instance!!!!!
     var createInstance = function () {
         return {
             say: function () {
-                alert("I am the greatest");
+                alert("It is just little ole me. The only instance of me!");
             }
         };
     }
-        
+    // Create private method to get instance and if it doesn't exist create one. 
     var getInstance = function () {
         return instance = instance || createInstance();
     }
-
+    // Create public method for getinstance 
+    // actually REVEALing the private getInstance. 
+    // (LOL ==> You see what I did there?)
     return {
         getInstance: getInstance
     };
 
 })();
-
-
-function run() {
-
-    var singleton = Patterns.Classic.Singleton;
-
+//==============================================    
+// run_OptimizedSingleton()
+//============================================== 
+function run_OptimizedSingleton() {
+    // create instance of Singleton module
+    var singleton = JsGangOfFour.Classic.Singleton;
+    // separately create two instance variables 
+    // using the getInstance method in our module
     var instance1 = singleton.getInstance();
     var instance2 = singleton.getInstance();
 
-    instance1.say();                        // => I am the greatest!
-
-    alert("Same instance? " + (instance1 === instance2)); // => true
+    instance1.say();    // => First AlertBox says: It is just little ole me.
+    instance2.say();    // => Second AlertBox says: It is just little ole me.
+    // AlertBox with results comparing whether both instances are the same.
+    alert("Result of test to see if instance1 and instance2 are the same? "
+        + (instance1 === instance2)); // => true
 }
