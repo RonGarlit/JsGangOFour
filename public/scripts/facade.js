4467713486c3516b28925770cc8940b17f32ffe3
RonGarlit <rongarlit@live.com>
2018-09-18 14:16:43 -0400

    Edit Decorator pattern and started Facade Pattern

--- public/scripts/facade.js
+++ public/scripts/facade.js
@@ -1,5 +1,36 @@
+/*
+***************NOTES Remove once code is wwritten***************
+Provide a unified interface to a set of interfaces in a subsystem. Facade defines a higher-level interface that makes the subsystem easier to use.
+
+
+The Façade pattern provides an interface which shields clients from complex functionality in one or more subsystems. It is a simple pattern that may seem trivial but it is powerful and extremely useful. It is often present in systems that are built around a multi-layer architecture.
+
+The intent of the Façade is to provide a high-level interface (properties and methods) that makes a subsystem or toolkit easy to use for the client.
+
+On the server, in a multi-layer web application you frequently have a presentation layer which is a client to a service layer. 
+
+Communication between these two layers takes place via a well-defined API. This API, or façade, hides the complexities of the business objects and their interactions from the presentation layer.
+
+Another area where Façades are used is in refactoring. Suppose you have a confusing or messy set of legacy objects that the client should not be concerned about. 
+
+You can hide this code behind a Façade. The Façade exposes only what is necessary and presents a cleaner and easy-to-use interface.
+
+Facades are a structural pattern that can often be seen in JavaScript libraries such as jQuery where, although an implementation may support methods with a wide range of behaviors, only a “facade,” or limited abstraction of these methods, is presented to the public for use.
+
+
+https://www.safaribooksonline.com/library/view/learning-javascript-design/9781449334840/ch09s09.html
+
+https://anasshekhamis.com/2017/09/21/facade-design-pattern-in-javascript/
+
+http://www.discoversdk.com/blog/javascript-facade-pattern
+
+https://www.joezimjs.com/javascript/javascript-design-patterns-facade/
+
+*/
+
+
 //==============================================
-// decorator.js
+// facade.js
 //==============================================
 // log helper
 // Used to add() to var log
@@ -15,7 +46,7 @@ var log = (function () {
 })();
 
 //==============================================
-// Decorator
+// Facade
 // JavaScript does not support class-based 
 // inheritance therefore the abstract classes.
 // we must ensure this consistency ourselves
@@ -28,48 +59,76 @@ var log = (function () {
 // without breaking things.
 //----------------------------------------------
 
-// Create the User object we will
-// later decorate with more properties
-var User = function (name) {
+var Mortgage = function (name) {
     this.name = name;
-    this.say = function () {
-        log.add("User: \n Name: " + this.name);
-    };
 }
 
-// Now pass the User object to the "decorator" object
-// that we created.
-var DecoratedUser = function (user, street, city) {
-    this.user = user;
-    this.name = user.name;  // ensures interface stays the same
-    this.street = street;
-    this.city = city;
-    this.say = function () {
-        log.add("Decorated User: \n Name: " + this.name + ", \n Street: " +
-            this.street + ", \n City: " + this.city);
-    };
+Mortgage.prototype = {
+    applyFor: function (amount) {
+
+        // access multiple subsystems...
+
+        var result = "approved";
+        if (!new Bank().verify(this.name, amount)) {
+            result = "denied";
+        } else if (!new Credit().get(this.name)) {
+            result = "denied";
+        } else if (!new Background().check(this.name)) {
+            result = "denied";
+        }
+
+        return this.name + " has been " + result +
+            " for a " + amount + " mortgage";
+    }
 }
+
+var Bank = function () {
+    this.verify = function (name, amount) {
+        // complex logic ...
+        var loadAndDoSomething = amount;
+        return true;
+    }
+}
+var Credit = function () {
+    this.get = function (name) {
+        // complex logic ...
+        var loadNameandDoSomething = name;
+        return true;
+    }
+}
+var Background = function () {
+    this.check = function (name) {
+        // complex logic ...
+        var loadNameandDoSomethingElse = name;
+        // change for fail on a name equal something
+        return true;
+    }
+}
+
+
+
 
 //==============================================
 // run_Decorator()
 //==============================================
-function run_Decorator() {
+function run_Facade() {
 
     log.add("-------------------------");
     // add to log heler var 
     log.add("Create the User Object:");
     // add to log heler var 
     log.add("-------------------------");
-    var user = new User("Kelly");
-    user.say();
+    var mortgage = new Mortgage("Joan Templeton");
+    var result = mortgage.applyFor("$100,000");
+
+    log.add(result);
 
     log.add("-------------------------");
     // add to log heler var 
     log.add("Now decorate above User Object:");
     // add to log heler var 
     log.add("-------------------------");
-    var decorated = new DecoratedUser(user, "Broadway", "New York");
-    decorated.say();
+
     // Display what wwe have in the log object
     log.show();
 }
@@ -104,7 +163,7 @@ var JsGangOfFour = {
     }
 };
 
-JsGangOfFour.namespace("Classic").Decorator = (function () {
+JsGangOfFour.namespace("Classic").Facade = (function () {
     //==============================================
     // JavaScript does not support class-based 
     // inheritance therefore the abstract classes.
@@ -128,55 +187,22 @@ JsGangOfFour.namespace("Classic").Decorator = (function () {
     //==============================================
     // extend method to add on to and existing object
     //==============================================
-    var extend = function (dest, source) {
-        for (var prop in source) {
-            if (source.hasOwnProperty(prop)) {
-                dest[prop] = source[prop];
-            }
-        }
-    };
-    //==============================================
-    // extendDeep method to add on to and existing object
-    // Add another layer of decorations?  :-)
-    //==============================================
-    var extendDeep = function (dest, source) {
-        for (var prop in source) {
-            if (source.hasOwnProperty(prop)) {
-                if (typeof prop === "object") {
-                    dest[prop] = $.isArray(prop) ? [] : {};
-                    this.deepExtend(dest[prop], source[prop]);
-                }
-                else {
-                    dest[prop] = source[prop];
-                }
-            }
-        }
-    };
 
-    return {
-        extend: extend,
-        extendDeep: extendDeep
-    };
 })();
 
 //==============================================    
 // run_OptimizedComposite()
 //============================================== 
-function run_OptimizedDecorator() {
+function run_OptimizedFacade() {
 
-    var decorator = JsGangOfFour.Classic.Decorator;
+    var facade = JsGangOfFour.Classic.Facade;
     // add to log heler var 
     log.add("-------------------------");
     // add to log heler var 
     log.add("Create the User Object:");
     // add to log heler var 
     log.add("-------------------------");
-    var User = function (name) {
-        this.name = name;
-        this.say = function () {
-            log.add("--> User: " + this.name);
-        };
-    }
+
     // Create user object we will decorate/extend
     var user = new User("Kelly");
     // add to log heler var 
@@ -188,35 +214,14 @@ function run_OptimizedDecorator() {
 
     // add to log heler var 
     log.add("Now decorate (using extend(dest, source) method) above User Object:");
-    decorator.extend(user, {
-        street: "Broadway",
-        city: "New York",
-        say: function () {
-            log.add("--> Decorated User: \n|---> Name: " + this.name + ", \n|---> Street: " +
-                this.street + ", \n|---> City: " + this.city);
-        }
-    });
+
     // add to log heler var with user.say()
     user.say();
     // add to log heler var 
     log.add("-------------------------")
     // add to log heler var 
     log.add("Now decorate (using extendDeep(dest, source) method) adding another layer of previously modified User Object:");
-    decorator.extendDeep(user, {
-        school: "Columbia",
-        grades: {
-            "Spring": 4.0,
-            "Fall": 3.5
-        },
-        say: function () {
-            log.add("--> Deeply Extended User: \n|---> Name: " + this.name + ", \n|---> Street: " +
-                this.street + ", \n|---> City: " + this.city + ", \n|---> School: " +
-                this.school + ", \n|---> Grades: \n|------> Spring: " +
-                this.grades.Spring + ", \n|------> Fall: " + this.grades.Fall);
-        }
-    });
-    // add to log heler var with user.say()    
-    user.say();
+
     // Display what wwe have in the log object
     log.show();
 }
